e1e7a03492f4894d2b1bf124030601b69079a23b
plowpath <plowpath@protonmail.com>
2020-11-12 19:13:44 +0300

    after hours finally seperated table view (damn js)

--- src/main.rs
+++ src/main.rs
@@ -46,7 +46,7 @@ impl Musteri {
 }
 
 #[get("/")]
-fn index() -> rocket::response::content::Html<std::string::String> {
+fn index() -> rocket::response::content::Json<std::string::String> {
     let conn = database::sqlite_connection();
     let a = database::data_hazirlama(&conn);
     let mut lel = String::new();
@@ -60,112 +60,7 @@ fn index() -> rocket::response::content::Html<std::string::String> {
     } else {
         lel += "]";
     }
-    //let zal = content::Json(lel);
-    let alsana1 = r#"<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, 
-                     initial-scale=1, 
-                     shrink-to-fit=no">
-      <title>
-        tüm liste
-      </title>
-    
-      <!-- Include Bootstrap for styling -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0/mdb.min.css">
-    
-      <!-- Include the Bootstrap Table CSS 
-      for the table -->
-      <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.css">
-    </head>
-    
-    <body>
-      <div class="container">
-        <h1 class="text text-success text-center ">
-          ogrenciapp
-        </h1>
-        <table class="table-striped border-success">
-          <thead>
-            <tr>
-              <th data-field="id">
-                <span class="text-success">
-                  id
-                </span>
-              </th>
-              <th data-field="isim">
-                <span class="text-success">
-                  isim
-                </span>
-              </th>
-              <th data-field="soyisim">
-                <span class="text-success">
-                  soyisim
-                </span>
-                <th data-field="telefon">
-                <span class="text-success">
-                  telefon
-                </span>
-                <th data-field="yemek">
-                <span class="text-success">
-                  yemek
-                </span>
-                <th data-field="servis">
-                <span class="text-success">
-                  servis
-                </span>
-                <th data-field="turkce">
-                <span class="text-success">
-                  turkce
-                </span>
-                <th data-field="matematik">
-                <span class="text-success">
-                  matematik
-                </span>
-                <th data-field="fen">
-                <span class="text-success">
-                  fen
-                </span>
-                <th data-field="sosyal">
-                <span class="text-success">
-                  sosyal
-                </span>
-              </th>
-            </tr>
-          </thead>
-        </table>
-      </div>
-    
-      <!-- Include jQuery and other required 
-      files for Bootstrap -->
-      <script src="https://code.jquery.com/jquery-3.3.1.min.js">
-      </script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js">
-      </script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/1.0.0/mdb.min.css">
-      </script>
-    
-      <!-- Include the JavaScript file 
-      for Bootstrap table -->
-      <script src="https://unpkg.com/bootstrap-table@1.16.0/dist/bootstrap-table.min.js">
-      </script>
-      <script type="text/javascript">
-        $(document).ready(function () {
-    
-         
-          $('table').bootstrapTable({
-            data: mydata
-          });
-        });
-    
-        var mydata ="#;
-    let alsana3 = r#";
-    </script>
-  </body>
-  
-  </html>"#;
-    content::Html(format!("{}{}{}", alsana1, lel, alsana3))
+    content::Json(lel)
 }
 
 #[get("/nuke")]
@@ -223,8 +118,14 @@ fn proper_data_insert(
 fn new() -> rocket::response::content::Html<std::string::String> {
     content::Html(fs::read_to_string("ui/new.html").expect("dosya yok"))
 }
+
+#[get("/table")]
+fn table() -> rocket::response::content::Html<std::string::String> {
+    content::Html(fs::read_to_string("ui/table.html").expect("dosya yok"))
+}
+
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index, proper_data_insert, nuke, new])
+        .mount("/", routes![index, proper_data_insert, nuke, new, table])
         .launch();
 }
